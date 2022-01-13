eb19cab70cb63fa872e2c55366735d5efe17b286
ydy4 <95150458+ydy4@users.noreply.github.com>
2022-01-13 03:46:33 +0100

    Add files via upload

---
+++ Raw-processus-fetch.ps1
@@ -0,0 +1,3 @@
+﻿get-content .\process.txt -ReadCount 1000 |
+foreach { $_ -match "Name=" } | sort | get-unique > fetched_processes.txt
+diff (cat .\fetched_processes.txt) (cat .\processes_windows.txt) > abnormale.txt
